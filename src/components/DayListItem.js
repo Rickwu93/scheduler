947ffafe2aa1b3af19f4184321bb2264c53dfdf7
Ricky <rickywu209@gmail.com>
2022-04-19 20:27:39 -0600

    integrated DayList for dynamic inputs

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "components/DayListItem.scss";
+const classNames = require('classnames');
 
 export default function DayListItem(props) {
+
+  function formatSpot () {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`
+    }
+  }
+
+  let dayClass = classNames("day-list__item", 
+  {"day-list__item--selected": props.selected, 
+  "day-list__item--full": props.spots === 0})
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpot()}</h3>
     </li>
   );
 }
+
