3ab45c8a91db1145f48657ef126e11a9a02c5fd5
Ricky <rickywu209@gmail.com>
2022-04-25 11:00:06 -0600

    bookInterview function created and passed in Application component as prop

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,7 +31,11 @@ export default function Application() {
       console.log(all, 'all')
         setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       })
-}, [])
+}, []);
+
+function bookInterview(id, interview) {
+  console.log(id, interview);
+}
 
 const dailyAppointments = getAppointmentsForDay(state, state.day);
 console.log(dailyAppointments, 'bbbb')
@@ -51,6 +55,7 @@ const schedule = dailyAppointments.map((appointment) => {
       time={appointment.time}
       interview={interview}
       interviewers={interviewers}
+      bookInterview={bookInterview}
     />
   );
 });
